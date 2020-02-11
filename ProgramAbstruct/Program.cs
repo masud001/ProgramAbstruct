46b6a9feb711d157390042a7246bdbd4a05f88f1
masud001 <madhnagar@gmail.com>
2020-02-12 02:05:29 +0600

    Banckaccount Program deleted from here

--- ProgramAbstruct/Program.cs
+++ ProgramAbstruct/Program.cs
@@ -2,29 +2,17 @@
 
 namespace ProgramAbstruct
 {
-
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Bank Account!");
-            CurrentBalance obj = new CurrentBalance();
-            obj.Deposit(500);
-            obj.Balance();
-            obj.Withdraw(1000);
-            
-
         }
     }
+
 }
 /*
 
-    INHERITENCE
-Bank Account Write a console program in C# program that can perform the 
-basic banking operations such as withdrawal, deposit, checking balance, etc on bank accounts. 
-There are two types of bank account. Savings account provides interest to the customer 
-which is added to balance at the rate of 10%. 
-Whereas Current account does not provide any interest 
-but a current account must have minimum balance of 100000 Taka.
+
      
 */
