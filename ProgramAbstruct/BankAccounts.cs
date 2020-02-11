cdf17ca48332e6b86c663208896903a4b7dec27c
masud001 <madhnagar@gmail.com>
2020-02-12 02:10:56 +0600

    Banckaccount Program done!

---
+++ ProgramAbstruct/BankAccounts.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProgramAbstruct
+{
+    class CurrentBalance
+    {
+        protected int balance = 100;
+        public int MainBalance()
+        {
+            Console.WriteLine("your Main Balance is : " + balance);
+            return balance;
+        }
+    }
+    class WithdrawAmount : CurrentBalance
+    {
+        protected int amount;
+        public bool Withdraw(int a)
+        {
+            amount = a;
+            Console.WriteLine("withdraw Amount is : " + amount);
+            if (balance >= amount)
+            {
+                balance -= amount;
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("widthdraw Amount is less than main balance: " + (balance - amount));
+            }
+
+            return false;
+        }
+    }
+    class DepositAmount : WithdrawAmount
+    {
+        public int Deposit(int d)
+        {
+            amount = d;
+            Console.WriteLine("Deposit amount is : " + amount);
+
+            return balance = (balance += amount);
+        }
+    };
+
+    class BankAccounts : DepositAmount
+    {
+        public int Balance()
+        {
+            return balance;
+        }
+    }
+}
+
--- ProgramAbstruct/Program.cs
+++ ProgramAbstruct/Program.cs
@@ -7,12 +7,18 @@ namespace ProgramAbstruct
         static void Main(string[] args)
         {
             Console.WriteLine("Bank Account!");
+            
         }
     }
-
 }
 /*
 
-
+    INHERITENCE
+Bank Account Write a console program in C# program that can perform the 
+basic banking operations such as withdrawal, deposit, checking balance, etc on bank accounts. 
+There are two types of bank account. Savings account provides interest to the customer 
+which is added to balance at the rate of 10%. 
+Whereas Current account does not provide any interest 
+but a current account must have minimum balance of 100000 Taka.
      
 */
